06ca4dd2d4dbbac4c9d3b4abe7e003ef022b6fb6
Marc Farra <marcfarra@gmail.com>
2025-05-13 22:01:30 +0300

    detect arabic numerals in sejjel and convert them.

--- components/SejjelInput.js
+++ components/SejjelInput.js
@@ -1,6 +1,24 @@
 import {h} from 'preact';
 import labels from '../i18n.json';
 
+// Function to detect if a string contains Arabic numerals
+const containsArabicNumerals = (input) => {
+  const arabicNumerals = /[٠-٩]/u;
+  return arabicNumerals.test(input);
+};
+
+// Function to convert Arabic numerals to standard numbers
+export const convertSejjelToStandard = (input) => {
+  if (!containsArabicNumerals(input)) return input;
+  
+  const arabicNumerals = {
+    '٠': '0', '١': '1', '٢': '2', '٣': '3', '٤': '4',
+    '٥': '5', '٦': '6', '٧': '7', '٨': '8', '٩': '9'
+  };
+  
+  return input.split('').map(char => arabicNumerals[char] || char).join('');
+};
+
 const SejjelInput = ({sejjel, onInput, lang}) => h(
   'div', {id: 'sejjelinput'},
   h('label', {}, labels[lang].labels.sejjel),
